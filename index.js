674e1044cb106ca30872ea0077232bd4dac36b38
John <johnnkonge2020@gmail.com>
2023-10-23 05:45:20 +0300

    trial

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+class polygon{
+    constructor(array){
+        this.array = array
+    }
+    get countSides(){
+      if(this.array >= 4){
+        console.log('This is a 4 sided polygon')
+      } 
+    }
+    get perimeter(){
+        a + b +c
+    }
+}
+
+class triangle extends polygon{
+    constructor(...args){
+        super(...args)
+    }
+    get isValid(){
+        if(this.array === 3){
+            console.log('This is a triangle')
+        }
+    }
+}
+
+class sq extends polygon{
+    constructor(...args){
+        super(...args)
+    }
+    get isValid(){
+        if(this.array === 4){
+            console.log('Its a valid square')
+        }
+    }
+
+    get area(){
+        console.log(this.array * this.array)
+    }
+}
+let square = new polygon([3,4,5,6])
+square.area
